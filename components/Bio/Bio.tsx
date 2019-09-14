4669840847214597330b6d81b01bdb110b59bffb
aromanarguello <aromanarguello@gmail.com>
2019-09-13 23:22:07 -0400

    Update Bio.tsx

--- components/Bio/Bio.tsx
+++ components/Bio/Bio.tsx
@@ -5,7 +5,7 @@ const Bio: React.FC = () => {
   return (
     <>
       <Text pb="20px">
-        I am a front-end engineer who just relocated to New York City. I currently work Venmo! More to come...
+        I am a front-end engineer who just relocated to New York City. I currently work for Venmo! More to come...
       </Text>
       <Text pb="20px">
         My hobbies include writing awesome components, learning new technologies
